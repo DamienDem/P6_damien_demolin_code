e82f82bd8c50b827f6f651456a38b36a0263ecb2
Damien Demolin <damien.demolin@gmail.com>
2021-12-10 21:41:02 +0100

    optimisation sauceSchema

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-  userId: { type: String, required: true },
-  name: { type: String, required: true },
-  manufacturer: { type: String, required: true},
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  heat: { type: Number, required: true },
-  likes: { type: Number, required: true },
-  dislikes : { type: Number, required: true },
-  usersLiked : {type: [String], required: true },
-  usersDisliked : {type: [String], required: true },
+  userId: { type: String, required: true, trim:true },
+  name: { type: String, required: true , trim:true },
+  manufacturer: { type: String, required: true, trim:true },
+  description: { type: String, required: true, trim:true },
+  mainPepper: { type: String, required: true, trim:true },
+  imageUrl: { type: String, required: true, trim:true },
+  heat: { type: Number, required: true, trim:true },
+  likes: { type: Number, required: true, trim:true },
+  dislikes : { type: Number, required: true, trim:true },
+  usersLiked : {type: [String], required: true, trim:true },
+  usersDisliked : {type: [String], required: true, trim:true },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
