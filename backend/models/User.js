36050474acdfcc37875bd3aed26ab2bc1cb18f68
Damien Demolin <damien.demolin@gmail.com>
2021-12-10 21:38:05 +0100

    optimisation userSchema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const { isEmail } = require('validator');
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  email: {
+    type: String, 
+    required: true,
+    unique: true,
+    validate: [isEmail],
+    lowercase: true,
+    trim: true,
+    lowercase: true 
+  },
+  password: { 
+    type: String, 
+    required: true, 
+    trim: true,
+    minLength: 6,
+    max: 250
+  }
 });
 
 userSchema.plugin(uniqueValidator);
